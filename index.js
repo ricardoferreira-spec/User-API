cf82e97266d20d5628235bfac43ed74d5c0c78af
ricardoferreira-spec <ricardoferreirafatec@yahoo.com.br>
2025-01-15 11:23:32 -0300

    inicial correto

--- index.js
+++ index.js
@@ -3,16 +3,80 @@ const app = express()
 app.use(express.json())
 
 let users = []
+let nextId = 1
 
+// Rota para retorna todos os usuários
 app.get("/users", (req, res) => {
-    res.send('Hello, World!')
+    res.json(users)
 })
 
+// Rota para criar um usuário
 app.post("/users", (req, res) => {
-    console.log(req.body)
     const { name, email } = req.body;
-    const newUser = 
+
+    if(!name || !email) {
+        return res.status(400).json({error: "Name e email são obrigatórios"})
+    }
+
+    const newUser = {
+        id: nextId++,
+        name: name,
+        email: email
+    }
+
+    users.push(newUser)
+    res.status(201).json(newUser)
 })
+
+// Rota para busca um usuário pelo ID
+app.get("/user/:id", (req, res) => {
+    const { id } = req.params
+    const user = users.find((u) => u.id === parseInt(id))
+
+    if(!user) {
+        return res.status(404).json({error: "Usuário não encontrado"})
+    }
+
+    res.json(user)
+})
+
+// Rota para deletar um usuário pelo ID
+app.delete("/user/:id", (req, res) => {
+    const { id } = req.params
+
+    const user = users.find((u) => u.id === parseInt(id))
+
+    if(!user) {
+        return res.status(404).json({error: "Usuário não encontrado"})
+    }
+
+    users = users.filter((u) => u.id != id)
+    res.status(204).send()
+})
+
+// Rota para atualizar o usuário pelo ID
+app.put("/user/:id", (req, res) => {
+    const { id } = req.params
+    const user = users.find((u) => u.id == id)
+
+    if(!user) {
+        return res.status(404).json({error: "Usuário não encontrado"})
+    }
+
+    const {name, email } = req.body
+
+    if(name){
+        user.name = name
+    }
+
+    if(email) {
+        user.email = email
+    }
+
+    res.json(user)
+})
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
