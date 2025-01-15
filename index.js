43f0ff6e07affd213ed37a4fc2610617585873ad
ricardoferreira-spec <ricardoferreirafatec@yahoo.com.br>
2025-01-15 14:32:26 -0300

    15-01-2025 Banco De dados
    Alterado para banco de dados

--- index.js
+++ index.js
@@ -5,16 +5,38 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-let users = []
-let nextId = 1
+//let users = [] // não precisamos mais destas variaveis, pois alteramos para banco de dados
+//let nextId = 1 // não precisamos mais destas variaveis, pois alteramos para banco de dados
 
-// Rota para retorna todos os usuários
-app.get("/users", (req, res) => {
+
+// Rota para busca de usuário conectado com banco de dados
+app.get("/users", async (req, res) => {                 //async porque banco assincrono. Aguarda o banco responder para continuar
+    const users = await prisma.user.findMany()
+
+   /* const { id } = req.params
+    const user = users.find((u) => u.id === parseInt(id))
+
+    if(!user) {
+        return res.status(404).json({error: "Usuário não encontrado"})
+    }*/
+
     res.json(users)
 })
 
+
+// Rota para busca um usuário pelo ID - Where id = 1
+app.get("/users/:id", async (req, res) => {             //async porque banco assincrono. Aguarda o banco responder para continuar
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({where:{id}})
+
+    if(!user) {
+        return res.status(404).json({error: "Usuário não encontrado"})
+    }
+    res.json(user)
+})
+
 // Rota para criar um usuário
-app.post("/users", async (req, res) => {        //acrescento async , assincrona, vai esperar o banco de dados confirmar para continuar
+app.post("/users", async (req, res) => {                //acrescento async , assincrona, vai esperar o banco de dados confirmar para continuar
     const { name, email } = req.body;
 
     if(!name || !email) {
@@ -34,40 +56,38 @@ app.post("/users", async (req, res) => {        //acrescento async , assincrona,
             email
         }
     })
-
     res.status(201).json(user)
 })
 
-// Rota para busca um usuário pelo ID
-app.get("/user/:id", (req, res) => {
-    const { id } = req.params
-    const user = users.find((u) => u.id === parseInt(id))
-
-    if(!user) {
-        return res.status(404).json({error: "Usuário não encontrado"})
-    }
-
-    res.json(user)
-})
-
 // Rota para deletar um usuário pelo ID
-app.delete("/user/:id", (req, res) => {
-    const { id } = req.params
-
-    const user = users.find((u) => u.id === parseInt(id))
+app.delete("/user/:id", async(req, res) => {            //async porque banco assincrono. Aguarda o banco responder para continuar
+    ///const { id } = req.params
+    ////const user = users.find((u) => u.id === parseInt(id))
+    /// trocando a Delete por Id por uma Delete no banco de dados
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({where:{id}})
 
     if(!user) {
         return res.status(404).json({error: "Usuário não encontrado"})
     }
 
-    users = users.filter((u) => u.id != id)
-    res.status(204).send()
+    ///users = users.filter((u) => u.id != id)
+    await prisma.user.delete({
+        where:{id}
+    })
+    res.status(204).send()    
 })
+
 
 // Rota para atualizar o usuário pelo ID
-app.put("/user/:id", (req, res) => {
-    const { id } = req.params
-    const user = users.find((u) => u.id == id)
+// alterado para atualizar o usuário no banco de dados pelo ID. Após alteração consultar no banco prisma para ver se alterou
+app.put("/user/:id", async (req, res) => {              //async porque banco assincrono. Aguarda o banco responder para continuar
+    
+    /*const { id } = req.params
+    const user = users.find((u) => u.id == id)  // find para verificar se usuário existe*/
+    
+    const id = parseInt(req.params.id)
+    const user = await prisma.user.findUnique({where:{id}})
 
     if(!user) {
         return res.status(404).json({error: "Usuário não encontrado"})
@@ -75,21 +95,28 @@ app.put("/user/:id", (req, res) => {
 
     const {name, email } = req.body
 
-    if(name){
+    const updatedUser = await prisma.user.update({
+        where: {id},
+        data:{
+            name,
+            email
+        }
+    })
+
+    res.json(updatedUser)
+
+    /*if(name){
         user.name = name
     }
-
     if(email) {
         user.email = email
     }
-
-    res.json(user)
+    res.json(user)*/
 })
-
-
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
 /// comando para rodar aplicação automaticamente após qualquer alteração no código: npx nodemon index.js
+/// 
